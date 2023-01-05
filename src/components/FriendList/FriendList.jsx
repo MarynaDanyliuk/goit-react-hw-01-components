ba2bed7bc75d8d2061f0d5837d8a208156877d6d
Mary Danyliuk <marydanyliuk@gmail.com>
2023-01-05 18:08:26 +0200

    components 4-task in progress

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,20 @@
-// import css from './FriendList.module.css';
-// import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+import PropTypes from 'prop-types';
 
 export const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
+    <ul className={css['friend-list']}>
       {/* <FriendListItem /> */}
       {/* <!-- Довільна кіл-сть FriendListItem --> */}
     </ul>
   );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
+  }),
 };
