6110c17a2c0bb1cadfdfd8314ef8cfecb550370e
Mary Danyliuk <marydanyliuk@gmail.com>
2023-01-05 14:13:55 +0200

    components 3-task in progress

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,11 @@
+// import css from './FriendList.module.css';
+// import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {/* <FriendListItem /> */}
+      {/* <!-- Довільна кіл-сть FriendListItem --> */}
+    </ul>
+  );
+};
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title = 'Upload stats', stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className="title">{title}</h2>
+
+      <ul className="stat-list">
+        <li className="item">
+          <span className="label">.docx</span>
+          <span className="percentage">4%</span>
+        </li>
+        <li className="item">
+          <span className="label">.mp3</span>
+          <span className="percentage">14%</span>
+        </li>
+        <li className="item">
+          <span className="label">.pdf</span>
+          <span className="percentage">41%</span>
+        </li>
+        <li className="item">
+          <span className="label">.mp4</span>
+          <span className="percentage">12%</span>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf({
+    id: PropTypes.number.isRequired,
+    label: PropTypes.number.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }),
+  // id: PropTypes.number.isRequired,
+};
